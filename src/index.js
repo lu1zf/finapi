4bdaaae60576e935c3ebb130f002bbf0011ae16e
Luiz Silva <luizsilva.ee@gmail.com>
2022-06-26 21:03:41 -0300

    feat(endpoint): Update account

--- src/index.js
+++ src/index.js
@@ -110,4 +110,13 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
   return response.status(201).send();
 });
 
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
 app.listen(3333);
