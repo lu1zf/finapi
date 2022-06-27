87b03607bae9c390efd937a798ed76f9ab622edb
Luiz Silva <luizsilva.ee@gmail.com>
2022-06-26 21:31:35 -0300

    feat(endpoint): Get balance

--- src/index.js
+++ src/index.js
@@ -139,4 +139,12 @@ app.get("/accounts", (request, response) => {
   return response.status(200).json(customers);
 });
 
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  return response.json(balance);
+});
+
 app.listen(3333);
